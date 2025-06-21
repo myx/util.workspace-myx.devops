d6bc90a2e538ab40645f26026911a7bdde962930
Alex Kharichev <myx@myx.nz>
2025-06-21 23:26:24 +0300

    * install script

--- sh-scripts/install-myx.devops-repository.sh
+++ sh-scripts/install-myx.devops-repository.sh
@@ -9,7 +9,6 @@
 set -e
 
 
-if false ; then
 FetchStdout() {
     : "${1:?"⛔ ERROR: FetchStdout: The URL is required!"}"
     set -e
@@ -40,83 +39,3 @@ FetchStdout https://raw.githubusercontent.com/myx/myx.distro-.local/refs/heads/m
 WORKSPACE
 
 exit 0
-fi
-
-
-ROOT_LIST="$( tr -s '[:space:]' ' ' \
-<<ROOT_LIST
-
-	lib
-	myx
-
-ROOT_LIST
-)" # ROOT_LIST
-
-REPO_LIST="$( cat \
-<<REPO_LIST
-
-	myx/util.workspace-myx.devops	git@github.com:myx/util.workspace-myx.devops.git	main
-
-REPO_LIST
-)" # REPO_LIST
-
-EXTRA_CMD="$( cat \
-<<REPO_LIST
-
-	# Run Distro tools from source folder, so we can edit source files and feel 
-	# the change without commiting and updating the system via public repository.
-
-	Distro DistroSourceTools --system-config-option --upsert-if MDLT_CONSOLE_ORIGIN source ""
-
-REPO_LIST
-)" # REPO_LIST
-
-: "${TGT_APP_PATH:=${1:?⛔ ERROR: TGT_APP_PATH env must be set or call as: $0 <workspace-path>}}"
-MMDAPP=$TGT_APP_PATH
-case $MMDAPP in
-  "~"*) MMDAPP=$HOME${MMDAPP#\~} ;;	# expand ~
-esac
-case $MMDAPP in
-  /*) ;;							# already absolute
-  *)  MMDAPP=$PWD/$MMDAPP ;;		# make absolute
-esac
-
-export MMDAPP
-
-mkdir -p "$MMDAPP"
-cd "$MMDAPP"
-
-echo "$0: Workspace root: $PWD" >&2
-
-if [ ! -d ".local/myx" ] ; then
-	echo "Install: .local system, pulling system packages..." >&2
-	mkdir -p ".local/myx" ; ( cd ".local/myx" ; rm -rf "myx.distro-.local" ; git clone git@github.com:myx/myx.distro-.local.git ) 
-fi
-if [ ! -d "source" ] ; then
-	echo "Install: DistroLocalTools.fn.sh --install-distro-source..." >&2
-	bash .local/myx/myx.distro-.local/sh-scripts/DistroLocalTools.fn.sh --install-distro-source
-fi
-
-sed -e 's/^[[:space:]]*//' -e '/^#/d' -e '/^$/d' | ./DistroSourceConsole.sh --non-interactive \
-<<SOURCE_SETUP
-
-	set -ex
-
-	echo "SourceInstall: Running task within Source Console..."  >&2
-
-	Source DistroSourceTools --register-repository-roots $ROOT_LIST
-  
-	echo "SourceInstall: Pull Initial Repositories..."  >&2
-	Source DistroImageSync --execute-from-stdin-repo-list <<REPO_LIST
-		$REPO_LIST
-	REPO_LIST
-
-	echo "SourceInstall: Sync All Known Projects..." >&2
-	Source DistroImageSync --all-tasks --execute-source-prepare-pull
-	
-	echo "SourceInstall: All Source Console tasks done."  >&2
-
-SOURCE_SETUP
-
-############
-echo "Done."
