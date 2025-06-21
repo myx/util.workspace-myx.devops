2d1e2623ebdaa2f9ddb6c1c65a2682f24e0ddcf2
Alex Kharichev <myx@myx.nz>
2025-06-22 00:46:41 +0300

    * install script

--- sh-scripts/install-myx.devops-repository.sh
+++ sh-scripts/install-myx.devops-repository.sh
@@ -8,7 +8,7 @@
 set -e
 
 FetchStdout() {
-    URL="${1:-"https://raw.githubusercontent.com/myx/myx.distro-.local/refs/heads/main/sh-scripts/workspace-install.sh"}"
+    local URL="${1:-"https://raw.githubusercontent.com/myx/myx.distro-.local/refs/heads/main/sh-scripts/workspace-install.sh"}"
     command -v curl  >/dev/null 2>&1 && { curl --silent -L "$URL"; return 0; }
     command -v fetch >/dev/null 2>&1 && { fetch -o - "$URL"; return 0; }
     command -v wget  >/dev/null 2>&1 && { wget --quiet -O - "$URL"; return 0; }
